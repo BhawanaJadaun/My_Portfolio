f0143b31d4b2288d0f43c498c9829fefa49c12b9
BhawanaJadaun <bhawanajadaun0001@gmail.com>
2024-06-26 12:53:51 +0530

    first commit

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -7,12 +7,14 @@ import './contact.css';
 function Contact() {
   const [result, setResult] = useState("");
 
+  const accessKey = process.env.REACT_APP_ACCESS_KEY;
+
   const onSubmit = async (event) => {
     event.preventDefault();
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "a6adbe23-61c1-4e76-b313-9c3a8b39aa74");
+    formData.append("access_key", accessKey);
 
     try {
       const response = await fetch("https://api.web3forms.com/submit", {
@@ -62,16 +64,95 @@ function Contact() {
         <form className="contact-form" onSubmit={onSubmit}>
           <input type="text" name="name" placeholder="Name" required />
           <input type="email" name="email" placeholder="Email" required />
-          <textarea name="message" placeholder="Message" rows={4} cols={5}required></textarea>
+          <textarea name="message" placeholder="Message" rows={4} cols={5} required></textarea>
           <button type="submit">Submit Form</button>
         </form>
         <div className="form-message">
-        <span>{result}</span>
-      </div>
-    
+          <span>{result}</span>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Contact;
+
+// import React, { useState } from 'react';
+// import Linkedin from '../../assets/linkedin.png';
+// import mail from '../../assets/email.jpg';
+// import github from '../../assets/github.png';
+// import './contact.css';
+
+// function Contact() {
+//   const [result, setResult] = useState("");
+
+//   const accessKey = process.env.REACT_APP_ACCESS_KEY;
+
+//   const onSubmit = async (event) => {
+//     event.preventDefault();
+//     setResult("Sending....");
+//     const formData = new FormData(event.target);
+
+//     formData.append("access_key", accessKey);
+
+//     try {
+//       const response = await fetch("https://api.web3forms.com/submit", {
+//         method: "POST",
+//         body: formData
+//       });
+
+//       const data = await response.json();
+
+//       if (data.success) {
+//         setResult("Form Submitted Successfully");
+//         event.target.reset();
+//       } else {
+//         console.error("Error", data);
+//         setResult(data.message);
+//       }
+//     } catch (error) {
+//       console.error("Error", error);
+//       setResult("An error occurred. Please try again.");
+//     }
+//   };
+
+//   return (
+//     <div className="contact-container">
+//       <div className="text-container">
+//         <h3>Let's Connect</h3>
+//         <h1>Get In Touch</h1>
+//         <p>
+//           I'm currently available to take on new projects, so feel free to send me a message about anything that you want to run past me. You can contact me anytime 24/7.
+//         </p>
+//         <div className="icon">
+//           <p>
+//             <img src={Linkedin} alt="LinkedIn" />
+//             LinkedIn
+//           </p>
+//           <p>
+//             <img src={mail} alt="Mail" />
+//             Mail
+//           </p>
+//           <p>
+//             <img src={github} alt="GitHub" />
+//             GitHub
+//           </p>
+//         </div>
+//       </div>
+//       <div className="contact-form-container">
+//         <form className="contact-form" onSubmit={onSubmit}>
+//           <input type="text" name="name" placeholder="Name" required />
+//           <input type="email" name="email" placeholder="Email" required />
+//           <textarea name="message" placeholder="Message" rows={4} cols={5}required></textarea>
+//           <button type="submit">Submit Form</button>
+//         </form>
+//         <div className="form-message">
+//         <span>{result}</span>
+//       </div>
+    
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Contact;
